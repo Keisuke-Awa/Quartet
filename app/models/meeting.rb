25b2d51d7509de13d5f2b37cf7f8892d248daff0
keita0724 <knakagawakeita@ezweb.ne.jp>
2020-12-05 14:25:04 +0900

    create meeting show action

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,3 +1,10 @@
 class Meeting < ApplicationRecord
+<<<<<<< Updated upstream
   belongs_to :user
+=======
+  belongs_to :planning_user, class_name: "User", foreign_key: "user_id"
+  belongs_to :place
+
+  # belongs_to :user
+>>>>>>> Stashed changes
 end
