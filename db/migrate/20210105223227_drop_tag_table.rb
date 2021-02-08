7cd1041dd1c033d1356bf1920e2690ca42fa513d
keita0724 <knakagawakeita@ezweb.ne.jp>
2021-02-08 17:06:47 +0900

    input sample data

--- db/migrate/20210105223227_drop_tag_table.rb
+++
@@ -1,7 +0,0 @@
-class DropTagTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :meal_type_tag_meetings
-    drop_table :meal_type_tags
-    drop_table :meal_type_categories
-  end
-end
--- db/migrate/20210109121121_remove_column_from_tags.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromTags < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :tags, :tag_category_id_id, :bigint, null: false
-  end
-end
