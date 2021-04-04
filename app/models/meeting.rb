cf1590b9145443330775348775080307cb9a381d
keita0724 <knakagawakeita@ezweb.ne.jp>
2021-04-04 18:02:39 +0900

    update layout and controller

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -10,11 +10,15 @@ class Meeting < ApplicationRecord
 
   scope :as_of_now, -> { where(meet_at: DateTime.now..Float::INFINITY) }
   scope :exclude_appointed, -> { where(appointment_id: nil) }
+  scope :with_planning_user, -> { eager_load(:planning_user) }
   scope :with_user_avatar, -> { eager_load(planning_user: {avatar_attachment: :blob}) }
   scope :exclude_current_user, -> (user) { where.not(planning_user_id: user.id) }
   scope :exclude_same_sex, -> (user) { where.not(users: {sex: user.sex}) }
   scope :with_place, -> { eager_load(:place) }
-  scope :paginate, -> (page) { page(page).per(100) }
+  scope :with_appointment, -> { eager_load(:appointment) }
+  scope :paginate, -> (page) { page(page).per(30) }
   scope :display_list, -> (user, page) { as_of_now.exclude_appointed.with_user_avatar.exclude_current_user(user)
                                               .exclude_same_sex(user).with_place.paginate(page) }
+  scope :search_with_appointment, -> (appointments) { where(appointment_id: appointments).with_user_avatar#.as_of_now
+                                        .with_place.with_appointment }
 end
