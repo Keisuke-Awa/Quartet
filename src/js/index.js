15d94788fc7dc365bd38054f430f1c84ec44e41c
keita0724 <knakagawakeita@ezweb.ne.jp>
2021-05-10 09:44:34 +0900

    modify layout

--- src/js/index.js
+++ src/js/index.js
@@ -39,6 +39,15 @@ $(function() {
   });
 })
 
+function flexTextarea(el) {
+  const dummy = el.querySelector('.flex-textarea__dummy')
+  el.querySelector('.flex-textarea__textarea').addEventListener('input', e => {
+    dummy.textContent = e.target.value + '\u200b'
+  })
+}
+
+document.querySelectorAll('.flex-textarea').forEach(flexTextarea)
+
 
 
 
